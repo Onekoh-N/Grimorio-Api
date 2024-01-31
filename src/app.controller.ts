b820f5a50aa2d2818109e6417426aa58e1cf66bc
Guanco <nicolaswanko95@gmail.com>
2024-01-31 01:43:21 -0300

    Elimino rutas innecesarias

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,15 +5,4 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/amor')
-  getPrueba(): string {
-    return "Hola Marce, te extraño";
-  }
-
-
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Puto el que lee';
+    return 'Hola Mundo';
   }
 }
